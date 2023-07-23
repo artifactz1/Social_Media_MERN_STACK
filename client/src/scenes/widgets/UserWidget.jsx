61e70af7a48c86b7a4df6e7330e347ab245f98eb
artifactz1 <punsalang.art@gmail.com>
2023-07-22 22:57:41 -0700

    User Widget : added image for twitter and linkedin

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -11,6 +11,8 @@ import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import twitterImg from "../../public/assets/twitter.png";
+import linkedinImg from "../../public/assets/linkedin.png";
 
 const UserWidget = ({ userId, picturePath }) => {
   // This is how we are goibng to grab the user from the backend
@@ -148,7 +150,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
         <FlexBetween gap="1rem" mb="0.5rem">
           <FlexBetween gap="1rem">
-            <img src="../assets/twitter.png" alt="twitter" />
+            <img src={twitterImg} alt="twitter" />
             <Box>
               <Typography color={main} fontWeight="500">
                 Twitter
@@ -161,7 +163,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
         <FlexBetween gap="1rem">
           <FlexBetween gap="1rem">
-            <img src="../assets/linkedin.png" alt="linkedin" />
+            <img src={linkedinImg} alt="linkedin" />
             <Box>
               <Typography color={main} fontWeight="500">
                 Linkedin
