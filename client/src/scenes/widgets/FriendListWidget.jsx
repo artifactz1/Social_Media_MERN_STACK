b1310f9bfa991abc4fb41f10509dca14e4507794
artifactz1 <punsalang.art@gmail.com>
2023-07-23 19:12:40 -0700

    Posts and Post Widget : finished section / added friends list / advertisment

---
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -0,0 +1,64 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+
+const FriendListWidget = ({ userId }) => {
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  // Making a function of getting friends so we can get a list of user friends
+  // The reason why we can't just grab it from user, the list of friends we are going to be using this list of friends widget
+  // if you click on someone elses profile
+  // This will grab the list of users for that specific user ID
+  // We need the make sure that we aren't just handling the current user but also for other users aswell
+
+  const getFriends = async () => {
+    // Making API call for user getting friends
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    // We are setting up our friends list in out state
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Box display="flex" flexDirection="column" gap="1.5rem">
+        {/* This is how we map out each friend component */}
+        {friends.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+          />
+        ))}
+      </Box>
+    </WidgetWrapper>
+  );
+};
+
+export default FriendListWidget;
